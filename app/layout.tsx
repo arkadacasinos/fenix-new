4195dfd5c19c1b6cdc7fe1ee818a70c70be6c442
arkadacasinos <bezdep.rf@mail.ru>
2026-06-08 01:59:00 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,7 +101,7 @@ export default function RootLayout({
   return (
     <html lang="ru" className={`${inter.variable} bg-background`} suppressHydrationWarning>
       <head>
-        <meta name="yandex-verification" content="" />
+        <meta name="yandex-verification" content="7ebb7a7a4c8b51f7" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <script
  dangerouslySetInnerHTML={{
